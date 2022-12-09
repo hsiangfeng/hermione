734469770d1393c1dd5810310a7c6e088c08cc49
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2022-12-09 10:20:50 +0800

    feat: 🎸 更新套件與修正程式碼給予預設值

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ client.once('ready', (c) => {
 client.on('messageCreate', async (msg) => {
   if (msg.author?.bot) return;
   if (msg.channel.parentId !== process.env.DISCORD_CHANNEL_ID) return;
-  let tempMsg = null;
 
+  let tempMsg = null;
   try {
     const replyMessage = '等我一下，我正在想要怎麼回覆你...';
     tempMsg = await msg.channel.send(replyMessage);
@@ -36,11 +36,11 @@ client.on('messageCreate', async (msg) => {
       tempContent += `${forum.content}\n`;
     });
 
-    const maxTextLength = process.env.MAX_TEXT_LENGTH;
+    const maxTextLength = process.env.MAX_TEXT_LENGTH || 1000;
 
     if (tempContent.length > maxTextLength) {
       await tempMsg.delete();
-      await msg.channel.send(`對不起 >_< 這個論壇內容已經超過 **${maxTextLength}** 字了！\n所以我無法在繼續回覆你了...\n建議你另外開啟論壇唷~`);
+      await msg.channel.send(`對不起 >_< 這個貼文內容已經超過 **${maxTextLength}** 字了！\n所以我無法在繼續回覆你了...\n建議你另外開啟新貼文唷~`);
       return;
     }
 
@@ -54,7 +54,7 @@ client.on('messageCreate', async (msg) => {
     console.log(error);
 
     await tempMsg.delete();
-    await msg.reply('對不起，我發生錯誤了，所以不知道該怎麼回你 QQ');
+    await msg.reply('對不起，我發生了不知名錯誤，所以沒辦法回覆你 QQ');
   }
 });
 
--- service/ai.js
+++ service/ai.js
@@ -10,19 +10,21 @@ const openAI = new OpenAIApi(configuration);
 
 async function openAiMessage(prompt) {
   try {
+    console.log(prompt);
     const { data } = await openAI.createCompletion({
-      model: process.env.OPEN_AI_GPT_MODEL,
+      model: process.env.OPEN_AI_GPT_MODEL || 'text-curie-001',
       prompt,
-      max_tokens: process.env.OPEN_AI_MAX_TOKENS,
+      max_tokens: process.env.OPEN_AI_MAX_TOKENS || 100,
       temperature: 0,
+      logprobs: 3,
     });
     const [choices] = data.choices;
 
     return choices.text.trim();
   } catch (error) {
     // eslint-disable-next-line no-console
-    console.error(error);
-    return 'oh！no！我不知道怎麼回覆你。';
+    // console.error(error);
+    return `對不起，我發生 **${error.response.status} - ${error.response.statusText}** 錯誤，所以不知道該怎麼回你 QQ`;
   }
 }
 
