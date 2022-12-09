00a2bc86805ec7e94346b840b2457e30db6bdfa5
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2022-12-09 10:33:00 +0800

    fix: 🐛 修正隱含轉型的錯誤

--- service/ai.js
+++ service/ai.js
@@ -13,7 +13,7 @@ async function openAiMessage(prompt) {
     const { data } = await openAI.createCompletion({
       model: process.env.OPEN_AI_GPT_MODEL || 'text-curie-001',
       prompt,
-      max_tokens: process.env.OPEN_AI_MAX_TOKENS || 100,
+      max_tokens: Number(process.env.OPEN_AI_MAX_TOKENS) || 100,
       temperature: 0,
       logprobs: 3,
     });
@@ -22,7 +22,7 @@ async function openAiMessage(prompt) {
     return choices.text.trim();
   } catch (error) {
     // eslint-disable-next-line no-console
-    // console.error(error);
+    console.error(error);
     return `對不起，我發生 **${error.response.status} - ${error.response.statusText}** 錯誤，所以不知道該怎麼回你 QQ`;
   }
 }
